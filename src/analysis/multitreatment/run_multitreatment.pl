b231226bf8770e9657b826ac0c1b285512df22de
Andy Ge <andyshge@gmail.com>
2025-07-15 14:13:43 -0400

    updated multitreatment

--- src/analysis/multitreatment/run_multitreatment.pl
+++ src/analysis/multitreatment/run_multitreatment.pl
@@ -27,97 +27,105 @@ sub main {
     my @fastq_selection1 = @{$config->{input}{selection_1}};
     my @fastq_control2 = (defined $config->{input}{control_2}) ? @{$config->{input}{control_2}} : ();
     my @fastq_selection2 = (defined $config->{input}{selection_2}) ? @{$config->{input}{selection_2}} : ();
-    my $size = @fastq_selection1;
-    for (my $i=0; $i<$size; $i++) {
-        my @commands = ();
-        my $control_1 = $fastq_control1[0];
-        my $selection_1 = $fastq_selection1[$i];
-        my $control_2;
-        my $selection_2;
-        my $prefix = $control_1;
-        $prefix =~ s/.1_val_1.fq.gz//; $prefix =~ s/.*\///g;
-        $prefix = $prefix."_".$i;
-        
-        # pass control1/2 and selection1/2 to process_fastq if 2 is empty str then we generate the file ourselves
-        if (defined $fastq_control2[0] && $fastq_control2[0] ne "") {
-            $control_1, $control_2 = process_fastq($fastq_control1[0], $fastq_control2[0], $prefix, $config->{dirs}{output}, $config->{commands}{do_trim});
-        } else {
-            $control_1, $control_2 = process_fastq($fastq_control1[0], "", $prefix, $config->{dirs}{output}, $config->{commands}{do_trim});
-        }
-        if (defined $fastq_selection2[$i] && $fastq_selection2[$i] ne "") {
-            $selection_1, $selection_2 = process_fastq($fastq_selection1[$i], $fastq_selection2[$i], $prefix, $config->{dirs}{output}, $config->{commands}{do_trim});
-        } else {
-            $selection_1, $selection_2 = process_fastq($fastq_selection1[$i], "", $prefix, $config->{dirs}{output}, $config->{commands}{do_trim});
-        }
-        # enforce in readme that if 2 file unspecified then must have same basename as 1 file with 1 replaced by 2
-        # pass both files into assembly and mapping
 
-        my $generic_control = $control_1;
-        my $generic_selection = $selection_1;
-        $generic_control =~ s/.1_val_1.fq.gz//; $generic_control =~ s/.*\///g;
-        $generic_selection =~ s/.1_val_1.fq.gz//; $generic_selection =~ s/.*\///g;
-        # Create commands
-        my $assembly_cmd = "perl analysis/multitreatment/multi_assembly.pl".
-            " --control-reads-1 ".$control_1.
-            " --control-reads-2 ".$control_2.
-            " --selection-reads-1 ".$selection_1.
-            " --selection-reads-2 ".$selection_2.
-            " --selection-num ".$i.
-            " --output-dir ".$config->{dirs}{output};
-        my $mapping_cmd = "perl mapping/mapping.pl".
-            " --control-reads-1 ".$control_1.
-            " --control-reads-2 ".$control_2.
-            " --selection-reads-1 ".$selection_1.
-            " --selection-reads-2 ".$selection_2.
-            " --prefix ".$prefix.
-            " --mapping-func ".$config->{mapping_func}.
-            " --output-dir ".$config->{dirs}{output};
-        my $annotation_cmd = "perl annotation/annotation.pl".
-            " --prefix ".$prefix.
-            " --output-dir ".$config->{dirs}{output};
-        my $enrichment_cmd = "perl enrichment/enrichment.pl".
-            " --generic-control ".$generic_control.
-            " --generic-selection ".$generic_selection.
-            " --prefix ".$prefix.
-            " --cutoff ".$config->{cutoff}.
-            " --output-dir ".$config->{dirs}{output};
+    # Join lists into comma-separated strings for passing to assembly
+    my ($control_1, $control_2);
+    if (@fastq_control2 && $fastq_control2[0] ne "") {
+        ($control_1, $control_2) = process_fastq($fastq_control1[0], $fastq_control2[0], $prefix, $config->{dirs}{output}, $config->{commands}{do_trim});
+    } else {
+        ($control_1, $control_2) = process_fastq($fastq_control1[0], "", $prefix, $config->{dirs}{output}, $config->{commands}{do_trim});
+    }
 
-        # Run assembly
-        my $assembly_pid = fork();
-        if ($assembly_pid == 0) {
-            # Child process for assembly
-            exec($assembly_cmd) if $config->{commands}{do_assembly};
-            exit(0);
+    my ($selection_1_str, $selection_2_str);
+    if (@fastq_selection2 && $fastq_selection2[0] ne "") {
+        # If selection2 files are provided, process and trim each pair
+        my @trimmed_selection1;
+        my @trimmed_selection2;
+        for (my $i = 0; $i < @fastq_selection1; $i++) {
+            my ($sel1, $sel2) = process_fastq($fastq_selection1[$i], $fastq_selection2[$i], $prefix."_sel".$i, $config->{dirs}{output}, $config->{commands}{do_trim});
+            push @trimmed_selection1, $sel1;
+            push @trimmed_selection2, $sel2;
         }
-
-        # Wait for assembly to complete before starting annotation and mapping
-        waitpid($assembly_pid, 0) if $config->{commands}{do_assembly};
-        
-        # Start mapping after assembly completes
-        my $mapping_pid = fork();
-        if ($mapping_pid == 0) {
-            # Child process for mapping
-            exec($mapping_cmd) if $config->{commands}{do_mapping};
-            exit(0);
+        $selection_1_str = join(",", @trimmed_selection1);
+        $selection_2_str = join(",", @trimmed_selection2);
+    } else {
+        # Only selection1 files, process and trim each
+        my @trimmed_selection1;
+         my @trimmed_selection2;
+        for (my $i = 0; $i < @fastq_selection1; $i++) {
+            my ($sel1, $sel2) = process_fastq($fastq_selection1[$i], "", $prefix."_sel".$i, $config->{dirs}{output}, $config->{commands}{do_trim});
+            push @trimmed_selection1, $sel1;
+            push @trimmed_selection2, $sel2;
         }
+        $selection_1_str = join(",", @trimmed_selection1);
+        $selection_2_str = join(",", @trimmed_selection1);
+    }
 
-        # Start annotation after assembly completes
-        my $annotation_pid = fork();
-        if ($annotation_pid == 0) {
-            # Child process for annotation
-            exec($annotation_cmd) if $config->{commands}{do_annotation};
-            exit(0);
-        }
+    my $prefix = $fastq_control1[0];
+    $prefix =~ s/.1_val_1.fq.gz//; $prefix =~ s/.*\///g;
 
-        # Wait for both mapping and annotation to complete
-        waitpid($mapping_pid, 0) if $config->{commands}{do_mapping};
-        waitpid($annotation_pid, 0) if $config->{commands}{do_annotation};
+    my $generic_control = $fastq_control1[0];
+    my $generic_selection = $fastq_selection1[0];
+    $generic_control =~ s/.1_val_1.fq.gz//; $generic_control =~ s/.*\///g;
+    $generic_selection =~ s/.1_val_1.fq.gz//; $generic_selection =~ s/.*\///g;
 
-        # Run enrichment after both mapping and annotation complete
-        if ($config->{commands}{do_enrichment}) {
-            system($enrichment_cmd) == 0 
-                or die "Failed to execute command: $enrichment_cmd";
-        }
+    # Create commands with full lists
+    my $assembly_cmd = "perl analysis/multitreatment/multi_assembly.pl".
+        " --control-reads-1 ".$control_1.
+        " --control-reads-2 ".$control_2.
+        " --selection-reads-1 ".$selection_1_str.
+        " --selection-reads-2 ".$selection_2_str.
+        " --output-dir ".$config->{dirs}{output};
+
+    my $mapping_cmd = "perl mapping/mapping.pl".
+        " --control-reads-1 ".$control_1.
+        " --control-reads-2 ".$control_2.
+        " --selection-reads-1 ".$selection_1_str.
+        " --selection-reads-2 ".$selection_2_str.
+        " --prefix ".$prefix.
+        " --mapping-func ".$config->{mapping_func}.
+        " --output-dir ".$config->{dirs}{output};
+
+    my $annotation_cmd = "perl annotation/annotation.pl".
+        " --prefix ".$prefix.
+        " --output-dir ".$config->{dirs}{output};
+
+    my $enrichment_cmd = "perl enrichment/enrichment.pl".
+        " --generic-control ".$generic_control.
+        " --generic-selection ".$generic_selection.
+        " --prefix ".$prefix.
+        " --cutoff ".$config->{cutoff}.
+        " --output-dir ".$config->{dirs}{output};
+
+    # Run assembly
+    my $assembly_pid = fork();
+    if ($assembly_pid == 0) {
+        exec($assembly_cmd) if $config->{commands}{do_assembly};
+        exit(0);
+    }
+    waitpid($assembly_pid, 0) if $config->{commands}{do_assembly};
+
+    # Start mapping after assembly completes
+    my $mapping_pid = fork();
+    if ($mapping_pid == 0) {
+        exec($mapping_cmd) if $config->{commands}{do_mapping};
+        exit(0);
+    }
+
+    # Start annotation after assembly completes
+    my $annotation_pid = fork();
+    if ($annotation_pid == 0) {
+        exec($annotation_cmd) if $config->{commands}{do_annotation};
+        exit(0);
+    }
+
+    waitpid($mapping_pid, 0) if $config->{commands}{do_mapping};
+    waitpid($annotation_pid, 0) if $config->{commands}{do_annotation};
+
+    # Run enrichment after both mapping and annotation complete
+    if ($config->{commands}{do_enrichment}) {
+        system($enrichment_cmd) == 0 
+            or die "Failed to execute command: $enrichment_cmd";
     }
 
     return 0;
