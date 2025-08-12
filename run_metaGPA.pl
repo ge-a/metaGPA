ad0f5bb47b1656f7030e063e1408dcdec7805dff
Andy Ge <andyshge@gmail.com>
2025-08-12 15:13:01 -0400

    remove loop

--- run_metaGPA.pl
+++ run_metaGPA.pl
@@ -25,108 +25,91 @@ sub main {
         $config->{commands}{do_enrichment} = "run";
     }
 
-    my @fastq_control1 = @{$config->{input}{control_1}};
-    my @fastq_selection1 = @{$config->{input}{selection_1}};
-    my @fastq_control2 = (defined $config->{input}{control_2}) ? @{$config->{input}{control_2}} : ();
-    my @fastq_selection2 = (defined $config->{input}{selection_2}) ? @{$config->{input}{selection_2}} : ();
-    my $size = @fastq_control1;
- 
-    for (my $i=0; $i<$size; $i++) {
-        my @commands = ();
-        my $control_1 = $fastq_control1[$i];
-        my $selection_1 = $fastq_selection1[$i];
-        my $control2;
-        my $selection_2;
-        my $prefix = $control_1;
-        $prefix =~ s/.1_val_1.fq.gz//; $prefix =~ s/.*\///g;
+    my $control_1 = $config->{input}{control_1};
+    my $selection_1 = $config->{input}{selection_1};
+    my $control2 = (defined $config->{input}{control_2}) ? $config->{input}{control_2} : "";
+    my $selection_2 = (defined $config->{input}{selection_2}) ? $config->{input}{selection_2} : "";
+    my $prefix = $control_1;
+    $prefix =~ s/.1_val_1.fq.gz//; $prefix =~ s/.*\///g;
         
-        # pass control1/2 and selection1/2 to process_fastq if 2 is empty str then we generate the file ourselves
-        if (defined $fastq_control2[$i] && $fastq_control2[$i] ne "") {
-            $control_1, $control2 = process_fastq($fastq_control1[$i], $fastq_control2[$i], $prefix, $config->{dirs}{output}, $config->{commands}{do_trim});
-        } else {
-            $control_1, $control2 = process_fastq($fastq_control1[$i], "", $prefix, $config->{dirs}{output}, $config->{commands}{do_trim});
-        }
-        if (defined $fastq_selection2[$i] && $fastq_selection2[$i] ne "") {
-            $selection_1, $selection_2 = process_fastq($fastq_selection1[$i], $fastq_selection2[$i], $prefix, $config->{dirs}{output}, $config->{commands}{do_trim});
-        } else {
-            $selection_1, $selection_2 = process_fastq($fastq_selection1[$i], "", $prefix, $config->{dirs}{output}, $config->{commands}{do_trim});
-        }
-        # enforce in readme that if 2 file unspecified then must have same basename as 1 file with 1 replaced by 2
-        # pass both files into assembly and mapping
+    # pass control1/2 and selection1/2 to process_fastq if 2 is empty str then we generate the file ourselves
+    $control_1, $control2 = process_fastq($control_1, $control2, $prefix, $config->{dirs}{output}, $config->{commands}{do_trim});
+    $selection_1, $selection_2 = process_fastq($selection_1, $selection_2, $prefix, $config->{dirs}{output}, $config->{commands}{do_trim});
+    # enforce in readme that if 2 file unspecified then must have same basename as 1 file with 1 replaced by 2
+    # pass both files into assembly and mapping
 
-        my $generic_control = $control_1;
-        my $generic_selection = $selection_1;
-        $generic_control =~ s/.1_val_1.fq.gz//; $generic_control =~ s/.*\///g;
-        $generic_selection =~ s/.1_val_1.fq.gz//; $generic_selection =~ s/.*\///g;  
+    my $generic_control = $control_1;
+    my $generic_selection = $selection_1;
+    $generic_control =~ s/.1_val_1.fq.gz//; $generic_control =~ s/.*\///g; # see if we can avoid this hard coded structure
+    $generic_selection =~ s/.1_val_1.fq.gz//; $generic_selection =~ s/.*\///g;  
 
-        my $assembly_path = File::Spec->catfile($Bin, "src", "assembly", "assembly.pl");
-        my $mapping_path = File::Spec->catfile($Bin, "src", "mapping", "mapping.pl");
-        my $annotation_path = File::Spec->catfile($Bin, "src", "annotation", "annotation.pl");
-        my $enrichment_path = File::Spec->catfile($Bin, "src", "enrichment", "enrichment.pl");
+    my $assembly_path = File::Spec->catfile($Bin, "src", "assembly", "assembly.pl");
+    my $mapping_path = File::Spec->catfile($Bin, "src", "mapping", "mapping.pl");
+    my $annotation_path = File::Spec->catfile($Bin, "src", "annotation", "annotation.pl");
+    my $enrichment_path = File::Spec->catfile($Bin, "src", "enrichment", "enrichment.pl");
 
-        # Create commands
-        my $assembly_cmd = "perl $assembly_path".
-            " --control-reads-1 ".$control_1.
-            " --control-reads-2 ".$control2.
-            " --selection-reads-1 ".$selection_1.
-            " --selection-reads-2 ".$selection_2.
-            " --threads ".$config->{threads}.
-            " --memory ".$config->{memory}.
-            " --output-dir ".$config->{dirs}{output};
-        my $mapping_cmd = "perl $mapping_path".
-            " --control-reads-1 ".$control_1.
-            " --control-reads-2 ".$control2.
-            " --selection-reads-1 ".$selection_1.
-            " --selection-reads-2 ".$selection_2.
-            " --prefix ".$prefix.
-            " --mapping-func ".$config->{mapping_func}.
-            " --output-dir ".$config->{dirs}{output};
-        my $annotation_cmd = "perl $annotation_path".
-            " --prefix ".$prefix.
-            " --output-dir ".$config->{dirs}{output};
-        my $enrichment_cmd = "perl $enrichment_path".
-            " --generic-control ".$generic_control.
-            " --generic-selection ".$generic_selection.
-            " --prefix ".$prefix.
-            " --cutoff ".$config->{cutoff}.
-            " --output-dir ".$config->{dirs}{output};
+    # Create commands
+    my $assembly_cmd = "perl $assembly_path".
+        " --control-reads-1 ".$control_1.
+        " --control-reads-2 ".$control2.
+        " --selection-reads-1 ".$selection_1.
+        " --selection-reads-2 ".$selection_2.
+        " --threads ".$config->{threads}.
+        " --memory ".$config->{memory}.
+        " --output-dir ".$config->{dirs}{output};
+    my $mapping_cmd = "perl $mapping_path".
+        " --control-reads-1 ".$control_1.
+        " --control-reads-2 ".$control2.
+        " --selection-reads-1 ".$selection_1.
+        " --selection-reads-2 ".$selection_2.
+        " --prefix ".$prefix.
+        " --mapping-func ".$config->{mapping_func}.
+        " --output-dir ".$config->{dirs}{output};
+    my $annotation_cmd = "perl $annotation_path".
+        " --prefix ".$prefix.
+        " --output-dir ".$config->{dirs}{output};
+    my $enrichment_cmd = "perl $enrichment_path".
+        " --generic-control ".$generic_control.
+        " --generic-selection ".$generic_selection.
+        " --prefix ".$prefix.
+        " --cutoff ".$config->{cutoff}.
+        " --output-dir ".$config->{dirs}{output};
 
-        # Run assembly
-        my $assembly_pid = fork();
-        if ($assembly_pid == 0) {
-            # Child process for assembly
-            exec($assembly_cmd) if $config->{commands}{do_assembly};
-            exit(0);
-        }
+    # Run assembly
+    my $assembly_pid = fork();
+    if ($assembly_pid == 0) {
+        # Child process for assembly
+        exec($assembly_cmd) if $config->{commands}{do_assembly};
+        exit(0);
+    }
 
-        # Wait for assembly to complete before starting annotation and mapping
-        waitpid($assembly_pid, 0) if $config->{commands}{do_assembly};
-        
-        # Start mapping after assembly completes
-        my $mapping_pid = fork();
-        if ($mapping_pid == 0) {
-            # Child process for mapping
-            exec($mapping_cmd) if $config->{commands}{do_mapping};
-            exit(0);
-        }
+    # Wait for assembly to complete before starting annotation and mapping
+    waitpid($assembly_pid, 0) if $config->{commands}{do_assembly};
+    
+    # Start mapping after assembly completes
+    my $mapping_pid = fork();
+    if ($mapping_pid == 0) {
+        # Child process for mapping
+        exec($mapping_cmd) if $config->{commands}{do_mapping};
+        exit(0);
+    }
 
-        # Start annotation after assembly completes
-        my $annotation_pid = fork();
-        if ($annotation_pid == 0) {
-            # Child process for annotation
-            exec($annotation_cmd) if $config->{commands}{do_annotation};
-            exit(0);
-        }
+    # Start annotation after assembly completes
+    my $annotation_pid = fork();
+    if ($annotation_pid == 0) {
+        # Child process for annotation
+        exec($annotation_cmd) if $config->{commands}{do_annotation};
+        exit(0);
+    }
 
-        # Wait for both mapping and annotation to complete
-        waitpid($mapping_pid, 0) if $config->{commands}{do_mapping};
-        waitpid($annotation_pid, 0) if $config->{commands}{do_annotation};
+    # Wait for both mapping and annotation to complete
+    waitpid($mapping_pid, 0) if $config->{commands}{do_mapping};
+    waitpid($annotation_pid, 0) if $config->{commands}{do_annotation};
 
-        # Run enrichment after both mapping and annotation complete
-        if ($config->{commands}{do_enrichment}) {
-            system($enrichment_cmd) == 0 
-                or die "Failed to execute command: $enrichment_cmd";
-        }
+    # Run enrichment after both mapping and annotation complete
+    if ($config->{commands}{do_enrichment}) {
+        system($enrichment_cmd) == 0 
+            or die "Failed to execute command: $enrichment_cmd";
     }
 
     return 0;
@@ -157,10 +140,10 @@ sub parse_arguments {
         "mapping|M" => \$config{commands}{do_mapping},
         "enrichment|E" => \$config{commands}{do_enrichment},
         "lite|l" => \$config{lite},
-        "control_1|c1=s@" => \$config{input}{control_1},
-        "selection_1|s1=s@" => \$config{input}{selection_1},
-        "control_2|c2=s@" => \$config{input}{control_2},
-        "selection_2|s2=s@" => \$config{input}{selection_2},
+        "control_1|c1=s" => \$config{input}{control_1},
+        "selection_1|s1=s" => \$config{input}{selection_1},
+        "control_2|c2=s" => \$config{input}{control_2},
+        "selection_2|s2=s" => \$config{input}{selection_2},
         "threads=i" => \$config{threads},
         "memory=i" => \$config{memory},
         "outdir|o=s" => \$config{dirs}{output},
